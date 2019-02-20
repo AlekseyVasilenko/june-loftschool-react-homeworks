c0b72b62631d7ea81e527a91202f906e9a0d1e7b
PrOOfyless <proofyless@gmail.com>
2019-02-20 21:53:48 +0300

    hw3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,93 +1,120 @@
-import React, {Component} from 'react';
-import './App.css';
-import PersonalForm from 'components/PersonalForm/PersonalForm';
-import CardForm from 'components/CardForm/CardForm';
-import Step from 'components/Step/Step';
+import React, { Component } from "react";
+import "./App.css";
+import PersonalForm from "../PersonalForm/PersonalForm";
+import CardForm from "../CardForm/CardForm";
+import Step from "../Step/Step";
 
 export default class App extends Component {
-   state = {
-      step: 1,
-      firstName: '',
-      lastName: '',
-      email: '',
-      cardNumber: ''
-   };
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    cardNumber: ''
+  };
 
-   handleClickNextForm = () => {
-      this.setState({
-         step: this.state.step + 1
-      })
-   };
+  handleClickNextForm = () => {
+    this.setState({
+      step: this.state.step + 1
+    });
+  };
 
-   handleTabClick = (tabNumber) => {
-      this.setState({
-         step: tabNumber
-      })
-   };
+  handleTabClick = tabNumber => {
+    this.setState({
+      step: tabNumber
+    });
+  };
 
-   handleChangeForm = (name, value) => {
-      this.setState({
-         [name]: value
-      });
-   };
+  handleChangeForm = (name, value) => {
+    this.setState({
+      [name]: value
+    });
+  };
 
-   isFormCommitable = () => {
-      switch (this.state.step) {
-         case 1:
-            return (this.state.firstName !== '' && this.state.lastName !== '' && this.state.email !== '' && this.state.email.includes('@'));
-         case 2:
-            return (this.state.cardNumber.length === 16);
-         default :
-            return false
-      }
-   };
+  validateEmail = (email) => {
+    let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    console.log(re.test(email));
+    return re.test(email);
+  };
 
-   renderForm = () => {
-      switch (this.state.step) {
-         case 1:
-            return <PersonalForm firstName={this.state.firstName} lastName={this.state.lastName} email={this.state.email} onChangeForm={this.handleChangeForm} />;
-         case 2:
-            return <CardForm cardNumber={this.state.cardNumber} onChangeForm={this.handleChangeForm} />;
-         case 3:
-            return <p data-test="congratulations">Поздравляем!</p>;
-         default:
-            return <p>Ошибка!</p>;
-      }
-   };
+  isFormCommitable = () => {
+    switch (this.state.step) {
+      case 1:
+        return (
+          this.state.firstName !== ''
+          && this.state.lastName !== ''
+          && this.state.email !== ''
+          && this.validateEmail(this.state.email)
+        );
+      case 2:
+        return (
+          this.state.cardNumber.length === 16
+        );
+      default :
+        return false;
+    }
+  };
 
-   render() {
-      const thisStep = this.state.step;
-      const stepTitles = ['Personal info', 'Card info', 'Success!'];
-      return (
-         <div className="container">
-            <div className="tab-panel">
-               {
-                  stepTitles.map((val, i) => {
-                     return (
-                        <Step number={++i}
-                              isSelected={thisStep === i}
-                              children={val}
-                              isClickable={thisStep > i}
-                              onClick={this.handleTabClick}
-                              key={i}
-                        />
-                     )
-                  })
-               }
-            </div>
+  renderForm = () => {
+    switch (this.state.step) {
+      case 1:
+        return (
+          <PersonalForm
+            firstName={this.state.firstName}
+            lastName={this.state.lastName}
+            email={this.state.email}
+            onChangeForm={this.handleChangeForm}
+          />
+        );
+      case 2:
+        return (
+          <CardForm
+            cardNumber={this.state.cardNumber}
+            onChangeForm={this.handleChangeForm}
+          />
+        );
+      case 3:
+        return <p data-test="congratulations">Поздравляем!</p>;
+      default:
+        return <p>Ошибка!</p>;
+    }
+  };
 
-            <div className="form-content">
-               {this.renderForm()}
-            </div>
-
-            <div className="button-panel">
-               <button className="button-next"
-                       onClick={this.handleClickNextForm}
-                       disabled={!this.isFormCommitable()}
-                       hidden={this.state.step === stepTitles.length}
-               >Next</button>
-            </div>
-         </div>
-      );
-   }
+  render() {
+    const thisStep = this.state.step;
+    const stepTitles = ["Personal info", "Card info", "Success!"];
+    return (
+      <div className="container">
+        <div className="tab-panel">
+          {
+            stepTitles.map((val, i) => {
+              return (
+                <Step
+                  key={i}
+                  number={++i}
+                  isSelected={thisStep === i}
+                  children={val}
+                  isClickable={thisStep > i}
+                  onClick={this.handleTabClick}
+                />
+              );
+            })
+          }
+        </div>
+        <div className="form-content">
+          {this.renderForm()}
+        </div>
+        <div className="button-panel">
+          <button
+            className="button-next"
+            onClick={this.handleClickNextForm}
+            disabled={!this.isFormCommitable()}
+            hidden={this.state.step === stepTitles.length}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,20 +1,26 @@
-import React, {Component} from 'react';
-import './CardForm.css';
+import React, { Component } from "react";
+import "./CardForm.css";
 
 export default class CardForm extends Component {
-   handleChangeForm = event => {
-      const {onChangeForm} = this.props;
-      onChangeForm(event.target.name, event.target.value);
-   };
+  handleChangeForm = event => {
+    const { onChangeForm } = this.props;
+    onChangeForm(event.target.name, event.target.value);
+  };
 
-   componentWillUnmount() {};
+  componentWillUnmount() {
+  };
 
-   render() {
-      return (
-         <div data-test="card-form" className="card-form">
-            <h1 className="title">Информация о карте</h1>
-            <input name="cardNumber" placeholder="0000 0000 0000 0000" value={this.props.cardNumber} onChange={this.handleChangeForm} />
-         </div>
-      );
-   }
+  render() {
+    return (
+      <div data-test="card-form" className="card-form">
+        <h1 className="title">Информация о карте</h1>
+        <input
+          name="cardNumber"
+          placeholder="0000 0000 0000 0000"
+          value={this.props.cardNumber}
+          onChange={this.handleChangeForm}
+        />
+      </div>
+    );
+  }
 }
