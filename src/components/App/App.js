dd4a850222c3f4082174318a0dc0a08218fd4b6c
PrOOfyless <proofyless@gmail.com>
2019-02-21 18:29:00 +0300

    hw3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -33,7 +33,6 @@ export default class App extends Component {
 
   validateEmail = (email) => {
     let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    console.log(re.test(email));
     return re.test(email);
   };
 
